8c4921c84e404c35592084d4ed73fd5ef1f81744
diogo-heleno <78448355+diogo-heleno@users.noreply.github.com>
2025-06-20 00:33:24 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 OPENAI_API_KEY = "sk-xxxxxx"
 OUTPUT_RSS_FILE = "mindfulnews.xml"
 OUTPUT_HTML_FILE = "mindfulnews.html"
-RUN_INTERVAL_HOURS = 2
-MAX_ARTICLES = 50
+RUN_INTERVAL_HOURS = 48
+MAX_ARTICLES = 100
